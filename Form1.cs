ee62b32d2961c691840a838ba199c2e31ad1bf43
Krosh93 <razar9361@gmail.com>
2023-05-30 19:13:18 +0500

    Добавьте файлы проекта.

---
+++ Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Задание_14
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        struct popul
+        {
+            public int num;
+            public int age;
+
+            public popul(int num, int age)
+            {
+                this.num = num;
+                this.age = age;
+            }
+        }
+        Random rnd = new Random();
+        popul[] nasel;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            int r = rnd.Next(15, 100);
+            nasel = new popul[r];
+
+            for (int i = 0; i < nasel.Length; i++)
+            {
+                
+                int a = rnd.Next(1930, 2024);
+
+                nasel[i] = new popul(i, a);
+                // У жителей можно добавить фамилию и имя.
+                listBox1.Items.Add($"Житель {i + 1}: {a}");
+            }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox2.Items.Clear();
+            int other = int.Parse(textBox1.Text);
+            listBox2.Items.Add($"Люди, родившиеся после {other}:");
+            int count = 0;
+            for (int i = 0; i < nasel.Length; i++)
+            {
+
+                int age = nasel[i].age;
+                if (age > other)
+                {
+                    listBox2.Items.Add($"Житель {i + 1}: {age}");
+
+                    count++;
+                    
+                }
+            }
+            MessageBox.Show($"Всего людей, родившихся после {other}: {count}");
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("Задание 14")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("Задание 14")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("97f50af4-0257-4be4-8de3-5df410a0c1de")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
